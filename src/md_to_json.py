6ba68dd1cd0b45d91adf0b1df6a432f1abfa0633
s2ward <conrad.toetterman@gmail.com>
2023-04-06 16:49:47 +0300

    Update md_to_json.py

--- src/md_to_json.py
+++ src/md_to_json.py
@@ -1,15 +1,12 @@
 import os
 import json
 
-print("Current working directory:", os.getcwd())
-
 def convert_md_to_json(folder):
     data = []
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(".md"):
                 filepath = os.path.join(root, file)
-                print(f"Opening file: {filepath}")
                 with open(filepath, "r") as f:
                     lines = f.readlines()
                 conversation = []
@@ -18,12 +15,21 @@ def convert_md_to_json(folder):
                         speaker, text = line.strip().split(":", 1)
                         conversation.append({speaker.strip(): text.strip()})
                 data.append({"file": filepath, "conversation": conversation})
-    
-    with open(os.path.join(os.path.abspath(os.path.join(folder, "..")), "conversations.json"), "w") as f:
-        json.dump(data, f)
-        print(f"Writing output file to: {os.path.join(os.path.abspath(folder), '..', 'conversations.json')}")
+    return data
 
 folders = [os.path.join("..", "npc", folder) for folder in os.listdir(os.path.join("..", "npc")) if os.path.isdir(os.path.join("..", "npc", folder))]
 
+all_data = []
+
 for folder in folders:
-    convert_md_to_json(folder)
+    all_data.extend(convert_md_to_json(folder))
+
+# Remove extra newlines
+for conv in all_data:
+    for line in conv["conversation"]:
+        speaker = list(line.keys())[0]
+        line[speaker] = line[speaker].replace("\n\n", "\n")
+
+with open(os.path.join("..", "npc", "conversations.json"), "w") as f:
+    json.dump(all_data, f)
+
